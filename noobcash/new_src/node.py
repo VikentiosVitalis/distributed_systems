0a4b0c899078031b9a86fc533f4beb4f7d54b19e
MarkRamosS <el18841@mail.ntua.gr>
2023-03-19 11:11:56 -0400

    Minor signature bugs.

--- noobcash/new_src/node.py
+++ noobcash/new_src/node.py
@@ -107,7 +107,7 @@ class Node:
         # Broadcast Transaction to everyone
         print("Broadcasting Transaction: ", transaction.tid)
         tmp = json.loads(transaction.toJSON())
-        print(tmp)
+        print(tmp.loads(tmp, indent=1))
         for ip in self.ipList:
             if ip[0] != self.id:
                 requests.post(ip[1] + "/broadcast", json=tmp, headers={

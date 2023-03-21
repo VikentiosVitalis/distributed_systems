f33c9d7d9d2dd528f1331d83d2a18cf24d8e247b
Vikentios Vitalis <62433719+VikentiosVitalis@users.noreply.github.com>
2023-03-21 19:22:37 +0200

    node.py

--- noobcash/new_src/node.py
+++ noobcash/new_src/node.py
@@ -71,7 +71,7 @@ class Node:
     def setGenesis(self, block):
         genesisblock = json.loads(block)
         print(genesisblock['transactions'])
-        t = json.loads(genesisblock['transactions'])    # Load the transaction
+        t = json.loads(genesisblock['transactions'][0])    # Load the transaction
         transaction = Transaction(
             t['sender'], t['receiver'], t['amount'], t['inputs'], t['amtLeft'], t['tid'], t['signature'])
         current_block = Block(

1e0cb02fa544b702e2b3ced7fd847010365ec5be
Vikentios Vitalis <62433719+VikentiosVitalis@users.noreply.github.com>
2023-03-24 18:42:31 +0200

    blockchain.py

---
+++ noobcash/etc/update.sh
@@ -0,0 +1,8 @@
+wget  https://github.com/VikentiosVitalis/distributed_systems/archive/master.zip
+unzip -o master.zip
+rm master.zip
+
+scp -r /home/user/distributed_systems-main/ user@snf-35159.ok-kno.grnetcloud.net:.
+scp -r /home/user/distributed_systems-main/ user@snf-35160.ok-kno.grnetcloud.net:.
+scp -r /home/user/distributed_systems-main/ user@snf-35161.ok-kno.grnetcloud.net:.
+scp -r /home/user/distributed_systems-main/ user@snf-35162.ok-kno.grnetcloud.net:.

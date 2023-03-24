f1cbc34a640544a4916ab44f4abb2c45a50246ae
Vikentios Vitalis <62433719+VikentiosVitalis@users.noreply.github.com>
2023-03-24 22:40:02 +0200

    createPlots.py

--- noobcash/createPlots.py
+++ noobcash/createPlots.py
@@ -1,16 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-diff = ['4','5']
+diff = ['4', '5']
 size = ['1', '5', '10']
 nodes = ['5', '10']
 
-tt = []
+t5 = []
+t10 = []
 for d in diff:
-    tt.append([])
+    t5.append([])
+    t10.append([])
     for s in size:
-        tt[-1].append([])
-        for n in nodes:
-            with open(f'times/mining{d}{s}{n}.txt') as f:
-                tt[-1][-1].append(f.readlines())
-print(tt)
+        with open(f'times/mining{d}{s}5.txt') as f:
+            t5[-1].append(np.average([float(x[:-1]) for x in f.readlines()]))
+        with open(f'times/mining{d}{s}10.txt') as f:
+            t10[-1].append(np.average([float(x[:-1]) for x in f.readlines()]))
+        
+
+print(t5)
+
+print(t10)
+plt.figure()
+plt.plot([1, 5, 10], t5[0])
+plt.plot([1, 5, 10], t10[0])
+plt.ylabel("Time taken")
+plt.title('Block mine times for difficulty 4')
+plt.savefig('plots/plot4.pdf')
+plt.figure()
+plt.plot([1, 5, 10], t5[1])
+plt.plot([1, 5, 10], t10[1])
+plt.title('Time taken')
+plt.title('Block mine times for difficulty 5')
+plt.savefig('plots/plot5.pdf')
+
+
+with open('times/time_total.txt') as f:
+    s = f.readlines()

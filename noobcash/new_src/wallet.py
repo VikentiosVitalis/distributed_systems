624b6e4a40078c6a98ac0a6888c48958bca04f30
Vikentios Vitalis <62433719+VikentiosVitalis@users.noreply.github.com>
2023-03-24 16:55:40 +0200

    wallet.py

--- noobcash/new_src/wallet.py
+++ noobcash/new_src/wallet.py
@@ -50,7 +50,7 @@ class Wallet:
     def getMoney(self, amount):
         if amount > self.getMyBalance():
             print("Not enough coins! ", self.getMyBalance())
-            return []
+            return [], amount
         tmp = 0
         transactions = []
         while tmp < amount:
